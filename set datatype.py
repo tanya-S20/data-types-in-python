721111e3d0a4f12bb76adca78e9bc1d0e04e68ad
tanya-S20 <142837716+tanya-S20@users.noreply.github.com>
2025-01-23 09:36:17 +0530

    Create set datatype.py

---
+++ set datatype.py
@@ -0,0 +1,14 @@
+# initializing empty set
+s1 = set()
+s2 = set(["Geeks", "For", "Geeks"])
+print("Set with the use of List: ", s2)
+
+set3 = set(["Geeks", "For", "Geeks"])
+print(set3)
+
+# loop through set
+for i in set3:
+    print(i, end=" ")
+    
+# check if item exist in set    
+print("Geeks" in set3)
